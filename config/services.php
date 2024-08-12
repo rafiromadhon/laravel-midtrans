d21a99e8c67f1b5c023274c0dcc6289fbfbc13d9
= <rafiromadhon006@gmail.com>
2024-08-12 22:26:16 +0700

    Updating serverkey & clientkey in service.php

--- config/services.php
+++ config/services.php
@@ -31,4 +31,12 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'midtrans' => [
+        'serverKey' => env('MIDTRANS_SERVERKEY'),
+        'clientKey' => env('MIDTRANS_CLIENTKEY'),
+        'isPorduction' => env('MIDTRANS_IS_PRODUCTION', false),
+        'isSanitized' => env('MIDTRANS_IS_SANITIZED', true),
+        'is3ds' => env('MIDTRANS_IS_3DS', true),
+    ],
+
 ];
